c940b1a01c888474a13179695408d887a0ae6a28
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-10-06 10:26:14 -0300

    chore: add vite, react, tailwind and shacdn setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+      <h1 className="mt-4 text-2xl font-bold bg-amber-400">Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
