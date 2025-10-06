955f93ee564b8994ea2672df36a6f11bb1a1998f
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-10-06 09:39:05 -0300

    feat: ensure that only authorized users can create, update or delete articles

--- src/modules/articles/articles.routes.ts
+++ src/modules/articles/articles.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { getArticlesHandler, createArticleHandler, getArticleByIdHandler, deleteArticleHandler, updateArticleHandler, } from "./articles.controller.js";
 import { authenticate } from "../../middleware/authenticate.js";
+import { authorize } from "../../middleware/authorize.js";
 
 const articlesRouter = Router();
 
 articlesRouter.get("/", getArticlesHandler);
 articlesRouter.get("/:id", getArticleByIdHandler);
-articlesRouter.post("/", authenticate, createArticleHandler);
-articlesRouter.put("/:id", authenticate, updateArticleHandler);
-articlesRouter.delete("/:id", authenticate, deleteArticleHandler);
+articlesRouter.post("/", [authenticate, authorize(["WRITER"])], createArticleHandler);
+articlesRouter.put("/:id", [authenticate, authorize(["WRITER"])], updateArticleHandler);
+articlesRouter.delete("/:id", [authenticate, authorize(["WRITER"])], deleteArticleHandler);
 
 export default articlesRouter;
